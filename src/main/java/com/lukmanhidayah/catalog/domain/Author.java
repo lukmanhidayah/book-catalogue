37015c294d87ccca8d9115e88f4b79ab812d39dc
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2024-08-26 15:59:35 +0700

    chore: Add birth date field to Author entity

--- src/main/java/com/lukmanhidayah/catalog/domain/Author.java
+++ src/main/java/com/lukmanhidayah/catalog/domain/Author.java
@@ -1,5 +1,13 @@
 package com.lukmanhidayah.catalog.domain;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,15 +15,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "author")
 public class Author {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(name = "name", nullable = false, columnDefinition = "varchar(300)")
 	private String name;
-	
-	private Long birthDate;
-	
-	
-	
+
+	@Column(name = "birth_date", nullable = false)
+	private LocalDate birthDate;
 
 }
